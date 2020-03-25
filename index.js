058bc1061f5ec77161e1fdb1c3480466ebdf8003
dieterjava <md_hermann@yahoo.de>
2020-03-25 21:26:53 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,33 +1,51 @@
-import React, { Component} from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
-import axios from 'axios';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Hello from "./Hello";
+import "./style.css";
+import axios from "axios";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: 'Dieter',
+      name: "Dieter",
       news_api: {
-        "status": "ok", "source": "time", "sortBy": "top", "articles":
-          [{ "author": "Lissandra Villa", "title": "Coronavirus Overshadows emergency.", "url": "http://time.com", "urlToImage": "https://api.time.com", "publishedAt": "2020-03-16T04:23:24Z" }
-          ]
+        status: "ok",
+        source: "time",
+        sortBy: "top",
+        articles: [
+          {
+            author: "Lissandra Villa",
+            title: "Coronavirus Overshadows emergency.",
+            url: "http://time.com",
+            urlToImage: "https://api.time.com",
+            publishedAt: "2020-03-16T04:23:24Z"
+          }
+        ]
       },
-      myArticles: [{ "author": "Charlotte Alter", "title": "Joe Biden Definitivel President", "description": "In the first one-on-one Democraticent.", "url": "http://time.com" }]
+      myArticles: [
+        {
+          author: "Charlotte Alter",
+          title: "Joe Biden Definitivel President",
+          description: "In the first one-on-one Democraticent.",
+          url: "http://time.com"
+        }
+      ]
     };
   }
 
   handleClick() {
-    console.log('button clicked');
-    console.log('this is:', this);
+    console.log("button clicked");
+    console.log("this is:", this);
     let self = this;
-    var URL = "https://newsapi.org/v2/everything?q=software&sortBy=publishedAt&apiKey=1d825f6378f2460d8bcf7edc35915e0f";
+    var URL =
+      "https://newsapi.org/v2/everything?q=software&sortBy=publishedAt&apiKey=1d825f6378f2460d8bcf7edc35915e0f";
 
-    axios.get(URL)
+    axios
+      .get(URL)
       //   axios.get('https://599d6a620a785b0011f4f6c8.mockapi.io/users')
       //   axios.get('https://jsonplaceholder.typicode.com/posts')
-      .then(function (response) {
+      .then(function(response) {
         // myString = JSON.parse(response.data);
         console.log(response);
         self.setState({
@@ -35,30 +53,42 @@ class App extends Component {
           myArticles: response.data.articles
         });
       })
-      .catch(function (error) {
+      .catch(function(error) {
         console.log(error);
       });
+           console.log("logging myArticles");
+    console.log("this is:",this);
+     console.table(this.state.news_api.articles);
+
   }
 
   render() {
-    console.log('render');
+    console.log("render");
+
     return (
       <div>
         <Hello name={this.state.name} />
         <button onClick={() => this.handleClick()}>
           Get News from newsapi.org
-      </button>
-        <br /><br />
+        </button>
+        <br />
+        <br />
 
-        {this.state.myArticles.map(item =>
-          <ul>{item.title}<br />
-            <a target="_blank" href={item.url}> {item.url}</a><br />
-            {item.description}</ul>
-
-        )}
+        {this.state.myArticles.map(item => (
+          <ul>
+            {item.title}
+            <br />
+            <a target="_blank" href={item.url}>
+              {" "}
+              {item.url}
+            </a>
+            <br />
+            {item.description}
+          </ul>
+        ))}
       </div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
